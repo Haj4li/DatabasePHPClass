de28ed084e4b582cf1e00f53cf07f41232f0e4b1
Ali Nakhaee <48994331+Haj4li@users.noreply.github.com>
2023-06-01 21:13:02 +0330

    Upload File :/

---
+++ dbapi.php
@@ -0,0 +1,29 @@
+<?php
+
+// Database API version 0.1
+// Coded By Ali Nakhaee
+
+class DataBase
+{
+    private $db;
+
+    public function Connect($db_name, $db_address, $db_username, $db_password)
+    {
+        $this->db = mysqli_connect($db_address, $db_username, $db_password, $db_name);
+        if (!$this->db) {
+            die("Database Connection Error : " . mysqli_connect_errno($this->db));
+        }
+        return mysqli_connect_errno($this->db);
+    }
+    public function Execute($query)
+    {
+        $result = mysqli_query($this->db, $query);
+        return array($result, mysqli_connect_errno($this->db));
+    }
+    public function Close()
+    {
+        mysqli_close($this->db);
+    }
+}
+
+?>
